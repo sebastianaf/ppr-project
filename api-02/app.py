45f13ef0281430bc46b6be151dd26d0d7fc82ef8
sebastianaf <sebastianafanador95@gmail.com>
2022-07-24 01:01:12 -0500

    changing api2

--- api-02/app.py
+++ api-02/app.py
@@ -1,77 +1,42 @@
-from flask import Flask, jsonify, request
-from models import app
-import numpy as np
-import re
-from desenfreno import Data
+#!flask/bin/python
+from flask_cors import CORS, cross_origin
+import os
+from flask import Flask, flash, request, redirect, url_for, jsonify
+from werkzeug.utils import secure_filename
+from os import system, getcwd
+import subprocess
 
-# Los datos que se deben enviar desde la Api hacia el backend deben tener el siguiente estilo: 
-# key           value
-# scenes        1 2 3 4; 1 2 3 4; 5 6 7 8; 4 2 1 3; 5 2 4 7
-# actors        actor1, actor2, actor3, actor4  , actor5 
-# duration      2,1,2,3,4
+ALLOWED_EXTENSIONS = {'dzn'}
 
-# El arreglo que se devolverá con la solución debe tener el siguiente estilo:
-desenfreno = [    
-        {"durations_scenes": [1, 2, 1, 3]},
-        {"Actors": [{
-            "Actor1": 
-            {
-                "scenes": 
-                    [1, -1, 1, 0], 
-                "price": 80
-            }
-        },
-        {
-            "Actor2": 
-            {
-                "scenes": 
-                    [1, -1, 1, 0],
-                "price": 95 
-            }
-        },]}    
-]
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/')
-def index():
-    return '''<title>Api Desenfreno</title>
-            <center><h1>Api Desenfreno</h1></center>'''
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/solve', methods=['POST'])
+@cross_origin()
+def upload_file():
+    models = ['models/Desenfreno.mzn']
+    result = "" 
+    target = os.path.join(getcwd(), 'upload') 
+    data = request.form['dznfile']
+    if data != '':
+        model = models[0]
+        with open(os.path.join(target,'Datos.dzn'),'w') as dznfile : dznfile.write(data)
+        command = "minizinc --solver COIN-BC " + os.path.join(getcwd(),'minizinc', model) + " " + os.path.join(target, 'Datos.dzn') + '>' + os.path.join(target, 'output.txt')
+        result = subprocess.Popen(command, shell=True)
+        result.wait()
+        f = open(os.path.join(target, 'output.txt'))
+        result = f.read()
+        return result
+    return "archivo invalido"
 
-@app.route("/all")
-def get_all_scenes():
-    """Por medio de esta función se debe enviar 
-        la solución al problema de restricciones"""
-    return jsonify(scenes_desenfreno=desenfreno)
+@app.route('/', methods=['GET'])
+def hi():
+    return "hello pyzinc"
 
-
-@app.route("/add", methods=["POST"])
-def post_scene():
-    """Aqui se ingresan los datos que van a ser 
-        llevados al modelo"""
-    actors = request.form.get("actors")
-    actors = re.sub(r"\s+", "", actors).split(',')
-    scenes = request.form.get("scenes")
-    scenes = np.matrix(scenes)
-    duration = request.form.get("duration")
-    duration = re.sub(r"\s+", "", duration).split(',')
-
-    availability = request.form.get("availability") # opcional
-    avoid = request.form.get("avoid") # opcional
-
-
-    if False: #availability and avoid:
-        availability = np.matrix(availability)
-        avoid = np.matrix(avoid)
-    else:
-        new_data = Data(actors, scenes, duration)
-        new_data.add_data()
-        #result = new_data()
-
-    # print(f"{actors}\n{scenes}\n{duration}")
-
-    return jsonify(response={"success": "Successfully added the new scenes."})
-
-
-##Se empieza la ejecución de la aplicación
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
--- api-02/models.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship
-
-app = Flask(__name__)
-
-
-# adicional
-# crear base de datos y modelos
